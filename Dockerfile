81b110c09b617d6431baa0966405d3005ecdb709
Focinfi <focinfi@gmail.com>
2017-04-27 21:24:20 +0800

    Info repo

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM daocloud.io/golang:1.8.1-onbuild
+RUN apt-get update && apt-get install -y git
+RUN go get github.com/Focinfi/sqs-node/
+RUN CGO_ENABLED=0 go install -a github.com/Focinfi/sqs-node/
+WORKDIR $GOPATH/src/github.com/Focinfi/sqs-node/
+EXPOSE 8080
+CMD ["sqs-node"]
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/Focinfi/sqs/node"
+)
+
+func main() {
+	addr := os.Getenv("SQS_NODE_ADDR")
+	masterAddr := os.Getenv("SQS_MASTER_ADDR")
+	if addr == "" || masterAddr == "" {
+		panic("Need SQS_NODE_ADDR and SQS_MASTER_ADDR environment parameters")
+	}
+
+	node.New(addr, masterAddr).Start()
+}
